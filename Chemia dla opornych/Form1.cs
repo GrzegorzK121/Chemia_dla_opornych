f2782437e87dc0fb36a467baed67b5217565b824
GrzegorzK121 <117106707+GrzegorzK121@users.noreply.github.com>
2022-10-31 18:21:30 +0100

    Add project files.

---
+++ Chemia dla opornych/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chemia_dla_opornych
+{
+    public partial class table1 : Form
+    {
+        public table1()
+        {
+            InitializeComponent();
+        }
+    }
+}
